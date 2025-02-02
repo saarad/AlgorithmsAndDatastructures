114812273a381c0ebf3bfa8621737a2123082c39
Ilia Rad Saadat <ilia.rad.saadat@hotmail.com>
2025-02-02 12:52:45 +0100

    Update 1752. Check if Array Is Sorted and Rotated.cs

--- 1752. Check if Array Is Sorted and Rotated.cs
+++ 1752. Check if Array Is Sorted and Rotated.cs
@@ -1,4 +1,11 @@
 //https://leetcode.com/problems/check-if-array-is-sorted-and-rotated
+/*
+Given an array nums, return true if the array was originally sorted in non-decreasing order, then rotated some number of positions (including zero). Otherwise, return false.
+
+There may be duplicates in the original array.
+
+Note: An array A rotated by x positions results in an array B of the same length such that A[i] == B[(i+x) % A.length], where % is the modulo operation.
+*/
 public class Solution {
     //We identify rotation point by where nums[i]>nums[i-1] - it is specified only 1 rotation
     //From that rotation point, if all numbers are non-decreasing order return true
